54d3150bce57b0b1703dd6fe7220312d803ea214
Vilius <vilius@vilius.net>
2024-05-06 14:20:40 +0300

    feat: animate logo, move to separate file

---
+++ src/components/AppLogo.vue
@@ -0,0 +1,41 @@
+<template>
+  <PButton class="logo-container flip-horizontal-bottom" text>
+    <span class="first-word">Mind</span>
+    <span cl class="second-word">Flip</span>
+  </PButton>
+</template>
+
+<style scoped>
+span {
+  color: white;
+}
+.logo-container {
+  padding: 8px 16px;
+}
+.flip-horizontal-bottom:hover {
+  animation: flip-horizontal-bottom 0.8s cubic-bezier(0.455, 0.03, 0.515, 0.955)
+    both;
+}
+.flip-horizontal-bottom:not(:hover) {
+  animation: flip-horizontal-top 0.8s cubic-bezier(0.455, 0.03, 0.515, 0.955)
+    reverse both;
+}
+
+@keyframes flip-horizontal-bottom {
+  0% {
+    transform: rotateX(0);
+  }
+  100% {
+    transform: rotateX(-360deg);
+  }
+}
+
+@keyframes flip-horizontal-top {
+  0% {
+    transform: rotateX(0);
+  }
+  100% {
+    transform: rotateX(-360deg);
+  }
+}
+</style>
